c63ad92df55c8b8cb645d94fb1bfae5f199e14a0
Yehia Abdul Hamid <yehiamcp@gmail.com>
2015-12-26 14:36:54 +0000

    add new operators, use white spaces

--- src/parsers/filter/Lexer.php
+++ src/parsers/filter/Lexer.php
@@ -4,7 +4,7 @@ class Lexer extends \yehiaHamid\easyParse\parsers\Lexer {
     // "field1 eq 'what ever', field2 nq 'term two'"
 
     protected static $_terminals = array(
-        "/^(eq|ne|lt|gt|like)/" => "T_OPERATOR",
+        "/^( eq | ne | lt | gt | like | le | ge | ilike )/" => "T_OPERATOR",
         "/^(\w+)/" => "T_WORD",
         "/^(\s+)/" => "T_WHITESPACE",
         "/^(?:(?:\"(?:\\\\\"|[^\"])+\")|(?:'(?:\\\'|[^'])+'))/" => "T_QUOTED", 
